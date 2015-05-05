d8a55b4d687b7f036760da94c13ebf3a2e9949ba
Ken Leidal <kkleidal@gmail.com>
2015-05-05 11:48:28 -0400

    added login support and pasting to a logged-in user's account

--- src/pastebin.js
+++ src/pastebin.js
@@ -13,7 +13,9 @@ var paste = function (key) {
 		  api_paste_name: params.title,
 		  //public = 0, unlisted = 1, private = 2
 		  api_paste_private: params.privacy != null ? params.privacy : 0,
-		  api_paste_expire_date: params.expire != null ? params.expire: 'N'
+		  api_paste_expire_date: params.expire != null ? params.expire: 'N',
+          api_paste_format: params.format || "text",
+          api_user_key: params.user_key || ""
 		});
 
 		var req = http.request({
@@ -42,6 +44,39 @@ var paste = function (key) {
 		req.write(query);
 		req.end();
 	};
+    this.login = function(user, pass, cb) {
+		var query = qs.stringify({
+		  api_dev_key: apikey,
+          api_user_name: user,
+          api_user_password: pass
+		});
+
+		var req = http.request({
+		  host: 'pastebin.com',
+		  port: 80,
+		  path: '/api/api_login.php',
+		  method: 'POST',
+		  headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': query.length
+		  }
+		}, function(res) {
+		  var data = '';
+		  res.on('data', function(chunk) {
+			data += chunk;
+		  });
+		  res.on('end', function() {
+		  if (cb != null) {
+			if (data.indexOf('Bad API request') > -1)
+				cb(data, null);
+			else
+				cb(null, data);
+		  }})
+		});
+
+		req.write(query);
+		req.end();
+    };
 };
 
 module.exports = function (apikey) { return new paste(apikey) };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
-var paste = require('../src/pastebin')('DEVKEY');
+var paste = require('../src/pastebin')('YOUR DEV KEY');
+var USERNAME = "A VALID USERNAME";
+var PASSWORD = "A VALID PASSWORD";
 
 describe('Basic Test.', function(){
   it('create a new bin', function(done){
@@ -7,5 +9,29 @@ describe('Basic Test.', function(){
 		assert.equal(err, null);
 		done()
 	});
+  });
+  it('login with invalid user/password fails', function(done) {
+      paste.login(USERNAME, PASSWORD + "fake", function(err, data) {
+          assert.equal(!! err, true);
+          assert.equal(!! data, false);
+          done();
+      });
+  });
+  it('login with valid user/password succeeds', function(done) {
+      paste.login(USERNAME, PASSWORD, function(err, data) {
+          assert.equal(!! err, false);
+          assert.equal(!! data, true);
+          done();
+      });
+  });
+  it('private paste with valid user/password succeeds', function(done) {
+      paste.login(USERNAME, PASSWORD, function(err, data) {
+          assert.equal(!! err, false);
+          assert.equal(!! data, true);
+          paste.new({title: 'test', content: 'test private post', user_key: data, privacy: 2}, function(err, ret) {
+              assert.equal(err, null);
+              done();
+          });
+      });
   });
 });
